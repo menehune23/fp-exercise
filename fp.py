2fb10af2b9987a847da62196f0a13c8bffe8309c
Andrew Meyer <anmeyer@nvidia.com>
2024-05-02 19:52:58 -0700

    Add solution

--- fp.py
+++ fp.py
@@ -1,18 +1,32 @@
 from typing import List
+from itertools import accumulate
+
+
+# This is a pure function
+def next_state(prev: str) -> str:
+    if prev == 'green':
+        return 'yellow'
+    
+    if prev == 'yellow':
+        return 'red'
+    
+    if prev == 'red':
+        return 'green'
+    
+    return 'invalid'
+
 
 
 def simulate_traffic_light(start: str, iterations: int) -> List[str]:
-    lights = []
-    state = start
+    ## Middle-ground: extracts most state management to pure function
+    # state = start
+    # lights = []
+    # 
+    # for _ in range(iterations):
+    #     lights.append(state)
+    #     state = next_state(state)
+    # 
+    # return lights
 
-    for _ in range(iterations):
-        lights.append(state)
-
-        if state == 'green':
-            state = 'yellow'
-        elif state == 'yellow':
-            state = 'red'
-        elif state == 'red':
-            state = 'green'
-    
-    return lights
+    # Most "functional"
+    return list(accumulate(range(iterations-1), lambda state, _: next_state(state), initial=start))
